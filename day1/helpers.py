9fcc11ceb32d9ed5cebfe64547f98d6468f5b4c8
Jeroen van Renen <jpvrenen@hotmail.com>
2020-12-01 11:02:07 +0100

    day1 numbers

--- day1/helpers.py
+++ day1/helpers.py
@@ -5,8 +5,8 @@ def subtract_and_find(number, data):
     :param data: list of numbers any 2 members sum to 'number'
     :return: numbers that sum 'number'
     """
-    for entry in data:
-        find_nr = int(number) - int(entry)
-        if find_nr in data:
-            return [entry, find_nr]
+    for first_nr in data:
+        second_nr = int(number) - int(entry)
+        if second_nr in data:
+            return [first_nr, second_nr]
     return []
