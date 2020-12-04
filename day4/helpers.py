2f3a8e4a4aae42f4b99dadbf631bb637abb68d30
Jeroen van Renen <jpvrenen@hotmail.com>
2020-12-04 16:12:50 +0100

    day4

--- day4/helpers.py
+++ day4/helpers.py
@@ -243,15 +243,17 @@ def passport_val_checks(passport):
 def valid_passport_advanced(passport):
     """
     Check if given passport is valid, returns True or False.
-    Passports are valid if they contain elements:
-    byr (Birth Year)
-    iyr (Issue Year)
-    eyr (Expiration Year)
-    hgt (Height)
-    hcl (Hair Color)
-    ecl (Eye Color)
-    pid (Passport ID)
-    cid (Country ID)  # missing cid counts as valid
+    Passports are valid when:
+    byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    hgt (Height) - a number followed by either cm or in:
+    If cm, the number must be at least 150 and at most 193.
+    If in, the number must be at least 59 and at most 76.
+    hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+    ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+    pid (Passport ID) - a nine-digit number, including leading zeroes.
+    cid (Country ID) - ignored, missing or not.
     :param passport: passport to validate
     :return: True when valid or False when invalid
     """
